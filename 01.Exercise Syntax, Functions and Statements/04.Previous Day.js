15308c9fd9143ccb8d8ba3a6288569e176c7990a
Pavel Pelovski <git.pelovski@gmail.com>
2024-10-06 12:48:18 +0300

    Ex.4 completed

---
+++ 01.Exercise Syntax, Functions and Statements/04.Previous Day.js
@@ -0,0 +1,23 @@
+// Write a JS function that calculates the date of the previous day by given year, month, and day.
+
+function calculatepreviousDay(year, month, day){
+    let previousDay = day - 1;
+
+    if(previousDay == 0){
+
+        month -=1;
+
+        if(month == 0){
+            year -= 1;
+            month = 12;
+        }
+
+        day = new Date(year, month, 0).getDate();
+    }else{
+        day -=1
+    }
+
+     return `${year}-${month}-${day}`
+}
+
+ console.log(calculatepreviousDay(2016, 10, 1));
