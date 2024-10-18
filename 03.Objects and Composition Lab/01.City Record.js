922283aa03cfd9b62cb1c3743224962c727e7cbe
Pavel Pelovski <git.pelovski@gmail.com>
2024-10-18 20:46:10 +0300

    Ex. 3 and 4 completed

--- 03.Objects and Composition Lab/01.City Record.js
+++ 03.Objects and Composition Lab/01.City Record.js
@@ -2,11 +2,7 @@
 // which you will need to set as properties of an object and return it.
 
 function objectConvertor(name, population, treasury){
-    return {
-        name,
-        population,
-        treasury,
-    };
+    return { name, population, treasury};
 }
 
 const output = objectConvertor('Tortuga',
