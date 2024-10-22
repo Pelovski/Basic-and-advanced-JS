445661366d5c80c15a1d62550923d5717f392af5
Pavel Pelovski <git.pelovski@gmail.com>
2024-10-22 20:35:38 +0300

    Ex 3 and 4 completed

--- 04. JS-Advanced DOM Introduction Lab/03.Sum Numbers/SumNumbers.js
+++ 04. JS-Advanced DOM Introduction Lab/03.Sum Numbers/SumNumbers.js
@@ -1,3 +1,12 @@
-function calc() {
-    // TODO: sum = num1 + num2
-}
+// Write a JS function that reads two numbers from input fields in a web page and puts their sum in another field when the user clicks on a button.
+// Input/Output
+// There will be no input/output, your program should instead modify the DOM of the given HTML document.
+
+
+ function calc() {
+     let firstNum = document.getElementById('num1');
+     let secondNum = document.getElementById('num2');
+     let sum = document.getElementById('sum');
+    
+     sum.value = Number(firstNum.value) + Number(secondNum.value);
+ }
--- 04. JS-Advanced DOM Introduction Lab/04.Show More/ShowMore.js
+++ 04. JS-Advanced DOM Introduction Lab/04.Show More/ShowMore.js
@@ -1,3 +1,7 @@
 function showText() {
-    // TODO
+    let element = document.getElementById('text');
+    let textMore = document.getElementById('more');
+
+    textMore.style.display = 'none';
+    element.style.display = 'inline';
 }
