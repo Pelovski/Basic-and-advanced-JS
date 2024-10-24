73645ca5fad292fcfbee0d6808f7c6a28d81294c
Pavel Pelovski <git.pelovski@gmail.com>
2024-10-24 15:53:31 +0300

    Ex. 3 and 4 completed

--- 04. JS-Advanced DOM Introduction Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ 04. JS-Advanced DOM Introduction Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,20 @@
+// An HTML file is given and your task is to show more/less information. By clicking the [More] button, 
+// it should reveal the content of a hidden div and changes the text of the button to [Less].
+// When the same link is clicked again (now reading Less), hide the div and change the text of the link to More. 
+// Link action should be toggleable (you should be able to click the button an infinite amount of times).
+
+
 function toggle() {
-    console.log('TODO:...');
-}
+  let buttonElement = document.querySelector('#accordion span');
+  let extraText = document.getElementById('extra');
+  let currentState = window.getComputedStyle(extraText).display;
+ 
+  if(currentState == 'none'){
+    extraText.style.display = 'inline';
+    buttonElement.textContent = 'LESS';
+  }else{
+    extraText.style.display = 'none';
+    buttonElement.textContent = 'MORE';
+  }
+  
+}   
--- 04. JS-Advanced DOM Introduction Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ 04. JS-Advanced DOM Introduction Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+   let townElements = document.querySelectorAll('#towns li');
+   let searchElement = document.getElementById('searchText');
+   let resultElement = document.getElementById('result');
+   let matches = 0;
+
+   for (const town of townElements) {
+
+      town.style.textDecoration = 'none';
+
+      if(town.textContent.toLowerCase().includes(searchElement.value.toLowerCase())){
+         town.style.textDecoration = 'underline';
+         town.style.fontWeight = 'bold';
+         matches++;
+      }
+   }
+   let matchesText = matches > 1 ? `${matches} matches found` : `${matches} match found`;
+   resultElement.textContent = matchesText;
+   
 }
