4650688af9ec24e28e33a287d9ee7d175664990e
Pavel Pelovski <git.pelovski@gmail.com>
2024-10-25 09:26:35 +0300

    Ex. 5 and 6 completed

--- 04. JS-Advanced DOM Introduction Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ 04. JS-Advanced DOM Introduction Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,20 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
+   let tableElements = document.querySelectorAll('tbody tr');
+   let searchTextElement = document.getElementById('searchField');
+   
    function onClick() {
-      //   TODO:
+     
+      for (let i = 0; i < tableElements.length; i++) {
+      
+         tableElements[i].classList.remove("select");
 
+         if(tableElements[i].textContent.toLowerCase().includes(searchTextElement.value.toLowerCase()) && searchTextElement.value != ''){
+            
+            tableElements[i].classList.add("select");
+         }
+         
+      }
    }
 }
--- 04. JS-Advanced DOM Introduction Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 04. JS-Advanced DOM Introduction Exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-  //TODO
+  let textAreaElement = document.getElementById('input');
+  let outputElement = document.getElementById('output');
+  let sentences = textAreaElement.value.split('.').filter(Boolean);
+  outputElement.innerHTML = "";
+  
+    for (let i = 0; i < sentences.length; i+=3) {
+      let paragraphText = sentences.slice(i, i + 3).join('.') + '.';
+      let paragraphElement = document.createElement('p');
+      paragraphElement.textContent = paragraphText;
+
+      outputElement.appendChild(paragraphElement);
+    }
+
+  textAreaElement.value = '';
+  
 }
