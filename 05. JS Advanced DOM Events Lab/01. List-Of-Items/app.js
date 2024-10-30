64ccc0937d2ca22b21305b62bd67b5846b923261
Pavel Pelovski <git.pelovski@gmail.com>
2024-10-30 21:00:48 +0200

    Ex.1 und 2 completed

--- 05. JS Advanced DOM Events Lab/01. List-Of-Items/app.js
+++ 05. JS Advanced DOM Events Lab/01. List-Of-Items/app.js
@@ -1,3 +1,13 @@
+//Write a function that reads the text inside an input field and appends the specified text to a list inside an HTML page.
+
 function addItem() {
-    console.log('TODO:...');
+    let inputTextElement = document.getElementById('newItemText');
+    let ulItemElement = document.getElementById('items');
+
+    let listItem = document.createElement('li');
+
+    listItem.textContent = inputTextElement.value;
+
+    ulItemElement.appendChild(listItem);
+    inputTextElement.value = '';   
 }
--- 05. JS Advanced DOM Events Lab/02. Delete-From-Table/app.js
+++ 05. JS Advanced DOM Events Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,23 @@
+// Write a program that takes an email from an input field and deletes the matching row from a table.
+
+// If entry is found, the textContent in the element with id="result" must be set to "Deleted."
+
+// Otherwise, an error should be displayed in a <div> with id="result". The error should be "Not found."
+
+// Submit only the deleteByEmail() function in Judge.
+
 function deleteByEmail() {
-    console.log('TODO:...');
+    let tdElements = document.querySelectorAll('#customers td:nth-child(even)');
+    let inputElement = document.querySelector('input');
+    let resultElement = document.getElementById('result');
+    resultElement.textContent = 'Not found.'
+
+    for (const element of tdElements) {
+        
+        if(inputElement.value == element.textContent){
+            element.parentElement.remove();
+            resultElement.textContent = 'Deleted.'
+        }
+    }
+    
 }
