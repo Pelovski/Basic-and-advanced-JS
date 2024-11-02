b02a32602d8ee67c7989071a33b4d29529ca6cba
Pavel Pelovski <git.pelovski@gmail.com>
2024-11-02 19:52:46 +0200

    Ex. 3 and 4 completed

--- 05. JS Advanced DOM Events Exercises/03. Locked-Profile/app.js
+++ 05. JS Advanced DOM Events Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let profileBtn = document.querySelectorAll('.profile button');
+
+    profileBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            let divElement = btn.parentElement.querySelector('div');
+            let ratioElement = btn.parentElement.querySelectorAll('input')[0];
+
+            if(!ratioElement.checked){
+
+          
+            if(btn.textContent == 'Show more'){
+               divElement.style.display = 'inline';
+               btn.textContent = 'Hide it'
+            }else{
+                btn.textContent = 'Show more';
+                divElement.style.display = 'none';
+            }
+        }
+        });
+    });
+    
 }
--- 05. JS Advanced DOM Events Exercises/04. Fill-Dropdown/dropdown.js
+++ 05. JS Advanced DOM Events Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemTextElement = document.getElementById('newItemText');
+    let newItemValueElement = document.getElementById('newItemValue');
+    let selectElement = document.getElementById('menu');
+    let optionElement = document.createElement('option');    
+
+    optionElement.value = newItemValueElement.value;
+    optionElement.text = newItemTextElement.value;
+
+    newItemTextElement.value = '';
+    newItemValueElement.value = '';
+
+    selectElement.appendChild(optionElement);
 }
