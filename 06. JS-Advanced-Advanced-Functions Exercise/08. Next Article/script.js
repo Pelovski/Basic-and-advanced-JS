91aa0528dd28f30ce252076e21fa391deb547a01
Pavel Pelovski <git.pelovski@gmail.com>
2024-11-12 21:40:22 +0200

    Ex.8 completed

--- 06. JS-Advanced-Advanced-Functions Exercise/08. Next Article/script.js
+++ 06. JS-Advanced-Advanced-Functions Exercise/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+
+   let content = document.getElementById('content');
+   let count = 0;
+    
+   return () => {
+        if(count < articles.length){
+        let articleElement = document.createElement('article');
+        articleElement.textContent = articles[count];
+        content.prepend(articleElement);
+        count++;
+    }
+   } 
 }
