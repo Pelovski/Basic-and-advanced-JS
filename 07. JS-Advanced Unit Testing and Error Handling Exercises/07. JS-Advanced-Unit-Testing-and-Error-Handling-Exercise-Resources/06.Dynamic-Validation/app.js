4162c9c7e089f0a1bfff726f0663d246f8342b09
Pavel Pelovski <git.pelovski@gmail.com>
2024-11-20 18:18:52 +0200

    Ex.6 completed

--- 07. JS-Advanced Unit Testing and Error Handling Exercises/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
+++ 07. JS-Advanced Unit Testing and Error Handling Exercises/07. JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    // TODO
+    let emailInput = document.getElementById('email');
+
+    emailInput.addEventListener('change', () =>{
+        const emailRegex = /^[a-z]+@[a-z]+\.[a-z]+$/;
+
+        if(!emailRegex.test(emailInput.value)){
+            emailInput.classList.add('error');
+        }else{
+            emailInput.classList.remove('error');
+        }
+    });
 }
