1581e899c48c7aa7aa7dc5047cac9ddbf429cb22
Pavel Pelovski <git.pelovski@gmail.com>
2025-01-13 23:44:25 +0200

    Ex.2 with promise and fetch completed

--- 01.Exercise Asynchronous Programming/02.Bus-Schedule/app.js
+++ 01.Exercise Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,11 +1,32 @@
 function solve() {
 
+    var next = 'depot';
+    var name = 'Depot';
+    let infoElement = document.querySelector('.info');
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
+    
+  
+    
     function depart() {
-        console.log('Depart TODO...');
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${next}`;
+         fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            next = data.next;
+            name = data.name;
+
+        infoElement.textContent = `Next Stop ${name}`;
+        departBtn.setAttribute('disabled', true);
+        arriveBtn.removeAttribute('disabled'); 
+
+        });  
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        departBtn.removeAttribute('disabled');
+        arriveBtn.setAttribute('disabled', true);
+        infoElement.textContent = `Arriving at ${name}`;
     }
 
     return {
