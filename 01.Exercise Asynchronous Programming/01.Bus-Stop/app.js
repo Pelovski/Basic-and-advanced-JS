627fa5f16cde5f8ef72107a27457525260d18cbc
Pavel Pelovski <git.pelovski@gmail.com>
2025-01-13 20:23:42 +0200

    Ex. 1 completed / Ex. with fatch and promises

--- 01.Exercise Asynchronous Programming/01.Bus-Stop/app.js
+++ 01.Exercise Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,3 +1,26 @@
 function getInfo() {
-    console.log("TODO...");
+    let busId = document.querySelector('#stopInfo input').value;
+    let stopNameElement = document.getElementById('stopName');
+    let busesElement = document.getElementById('buses');
+
+    stopNameElement.textContent = '';
+    busesElement.textContent = '';
+
+    let url = `http://localhost:3030/jsonstore/bus/businfo/${busId}`;
+
+    fetch(url)
+    .then(responce => responce.json())
+    .then(data => {
+        stopNameElement.textContent = data.name;
+
+      for (const key in data.buses) {
+        let liElement = document.createElement('li');
+        liElement.textContent = `Bus ${key} arrives in ${data.buses[key]} minutes`;
+        busesElement.appendChild(liElement);
+      }
+    })
+    .catch(err => {
+        stopNameElement.textContent = 'Error';
+    });
+
 }
